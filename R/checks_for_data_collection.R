a58673485eef179ea7a5028bcd9ddd2af6d2d8a1
Anthony Twesigye <twesigye10@gmail.com>
2022-08-04 10:35:01 +0300

    Time checks

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -19,3 +19,29 @@ df_survey <- readxl::read_excel(path = "inputs/livelihoods_assessment_tool.xlsx"
 df_choices <- readxl::read_excel(path = "inputs/livelihoods_assessment_tool.xlsx", sheet = "choices")
 
 df_sample_data <- sf::st_read("inputs/anif_rapid_settlement_samples.gpkg", quiet = TRUE)
+
+# output holder -----------------------------------------------------------
+
+logic_output <- list()
+
+# Time checks -------------------------------------------------------------
+
+# Time interval for the survey
+min_time_of_survey <- 20
+max_time_of_survey <- 120
+
+
+df_survey_time <- check_survey_time(input_tool_data = df_tool_data, 
+                                    input_min_time = min_time_of_survey,
+                                    input_max_time = max_time_of_survey)
+
+add_checks_data_to_list(input_list_name = "logic_output",input_df_name = "df_survey_time")
+
+# check the time between surveys
+min_time_btn_surveys <- 5
+
+df_time_btn_surveys <- check_time_interval_btn_surveys(input_tool_data = df_tool_data, 
+                                                       input_min_time = min_time_btn_surveys)
+
+add_checks_data_to_list(input_list_name = "logic_output",input_df_name = "df_time_btn_surveys")
+
