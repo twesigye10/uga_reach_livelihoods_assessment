03de05f1a56574dd96d6df49c6d4371de332c67d
atmathias <atmathias@gmail.com>
2022-08-20 17:34:06 +0300

    check:hh access to vsla loans

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -820,7 +820,28 @@ add_checks_data_to_list(input_list_name = "logic_output", input_df_name = "df_hh
 # HH reports haven taken out a loan from a VSLA OR having access to loans from a VSLA, but reports not having access to VSLAs i.e.
 # (name_fsp_hh_sought_loan = 'vsla' OR name_fsp_hh_to_seek_loan = 'vsla') AND not(selected(financial_service_providers_present, 'vsla'))
 
+df_hh_access_vsla_loans_28 <- df_tool_data %>% 
+  filter(str_detect(string = name_fsp_hh_sought_loan, pattern = "vsla") |
+           str_detect(string = name_fsp_hh_to_seek_loan, pattern = "vsla"),
+         !str_detect(string = financial_service_providers_present, pattern = "vsla")) %>% 
+  mutate(i.check.type = "remove_option",
+         i.check.name = "financial_service_providers_present",
+         i.check.current_value = as.character(financial_service_providers_present),
+         i.check.value = "",
+         i.check.issue_id = "logic_c_financial_service_providers_present_vsla_no_28",
+         i.check.issue = glue("name_fsp_hh_sought_loan: {name_fsp_hh_sought_loan} or name_fsp_hh_to_seek_loan: {name_fsp_hh_to_seek_loan} 
+                              but financial_service_providers_present has no 'vsla'selected as an option"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.so_sm_choices = "") %>% 
+  dplyr::select(starts_with("i.check.")) %>% 
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
 
+add_checks_data_to_list(input_list_name = "logic_output", input_df_name = "df_hh_access_vsla_loans_28")
 
 
 
