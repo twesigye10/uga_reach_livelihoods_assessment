10d7e14e3f3465c910756929fc8e2621e9da4972
atmathias <atmathias@gmail.com>
2022-08-27 10:43:45 +0300

    check:hh has more than one fridge

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -1365,8 +1365,7 @@ df_generator_greater_than_one_46 <- df_tool_data %>%
          i.check.current_value = as.numeric(cell_phone),
          i.check.value = "",
          i.check.issue_id = "logic_c_generator_greater_than_one_46",
-         i.check.issue = glue("generator: {generator}, 
-                              confirm hh has more than one generator"),
+         i.check.issue = glue("generator: {generator}, confirm hh has more than one generator"),
          i.check.other_text = "",
          i.check.checked_by = "",
          i.check.checked_date = as_date(today()),
@@ -1380,6 +1379,27 @@ df_generator_greater_than_one_46 <- df_tool_data %>%
 add_checks_data_to_list(input_list_name = "logic_output", input_df_name = "df_generator_greater_than_one_46")
 
 
+# Amount of fridges is unreasonable i.e. fridge  > 1  
+
+df_fridge_greater_than_one_47 <- df_tool_data %>% 
+  filter(fridge  > 1) %>% 
+  mutate(i.check.type = "change_response",
+         i.check.name = "fridge ",
+         i.check.current_value = as.numeric(cell_phone),
+         i.check.value = "",
+         i.check.issue_id = "logic_c_generator_greater_than_one_46",
+         i.check.issue = glue("fridge : {fridge}, confirm hh has more than one fridge"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.so_sm_choices = "") %>% 
+  dplyr::select(starts_with("i.check.")) %>% 
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+
+add_checks_data_to_list(input_list_name = "logic_output", input_df_name = "df_fridge_greater_than_one_47")
 
 
 
