e949924fac64959013b1c260e781cbf2c56710b1
Anthony Twesigye <twesigye10@gmail.com>
2022-10-05 16:20:20 +0300

    silhouette analysis

--- R/support_functions.R
+++ R/support_functions.R
@@ -19,6 +19,9 @@ get_average_survey_time <- function(input_tool_data) {
     pull()
 }
 
+
+# similarity analysis -----------------------------------------------------
+
 # for each survey, it finds the closest matching survey with the minimum number of different columns
 calculateDifferences <- function(data, tool.survey){
   # 1) convert all columns to character
@@ -46,4 +49,74 @@ calculateDifferences <- function(data, tool.survey){
   data[["number.different.columns"]] <- as.numeric(r)
   data <- data %>% arrange(number.different.columns, survey.id)
   return(data)
+}
+
+
+# silhouette analysis based on gower distance between surveys -------------
+
+calculateEnumeratorSimilarity <- function(data, tool.survey, col_enum, col_admin){
+  
+  # helper function
+  convertColTypes <- function(data, tool.survey){
+    # select_multiple: numeric or factor?
+    col.types <- data.frame(column=colnames(data)) %>% 
+      left_join(select(tool.survey, name, type), by=c("column"="name")) %>% 
+      mutate(type.edited = case_when(type %in% c("integer", "decimal", "calculate") ~ "numeric",
+                                     str_starts(type, "select_") ~ "factor",
+                                     str_detect(column, "/") ~ "factor",
+                                     TRUE ~ "text"))
+    
+    cols <- col.types[col.types$type.edited=="numeric", "column"]
+    data[,cols] <- lapply(data[,cols], as.numeric)
+    cols <- col.types[col.types$type.edited=="text", "column"]
+    data[,cols] <- lapply(data[,cols], as.character)
+    cols <- col.types[col.types$type.edited=="factor", "column"]
+    data[,cols] <- lapply(data[,cols], as.factor)
+    
+    return(data)
+  }
+  
+  # convert columns using the tool
+  data <- convertColTypes(data, tool.survey)
+  # keep only relevant columns
+  cols <- data.frame(column=colnames(data)) %>% 
+    left_join(select(tool.survey, name, type), by=c("column"="name")) %>% 
+    filter(!(type %in% c("_uuid", "enumerator_id")) &
+             !str_starts(column, "_other$") &
+             !str_detect(column, "_specify$"))
+  # convert character columns to factor and add enum.id
+  data <- data[, all_of(cols$column)] %>% 
+    mutate_if(is.character, factor) %>% 
+    arrange(!!sym(col_enum)) %>%
+    mutate(enum.id=as.numeric(!!sym(col_enum)), .after=!!sym(col_enum))
+  # add "SY" column in case col_admin is not specified
+  #if (col_admin=="adm") data <- mutate(data, adm="DRC", .before=cols$column[1])
+  # calculate similarity (for enumerators who completed at least 5 surveys)
+  res <- data %>% split(data[[col_admin]]) %>% 
+    lapply(function(gov){
+      df <- gov %>% 
+        group_by(enum.id) %>% 
+        mutate(n=n()) %>% 
+        filter(n>=5) %>% 
+        ungroup() %>% 
+        select_if(function(x) any(!is.na(x)))
+      
+      if (length(unique(df$enum.id)) > 1){
+        # calculate gower distance
+        gower_dist <- daisy(select(df, -c(!!sym(col_enum), enum.id)), 
+                            metric = "gower", warnBin = F, warnAsym = F, warnConst = F)
+        # gower_mat <- as.matrix(gower_dist)
+        # calculate silhouette
+        si <- silhouette(df$enum.id, gower_dist)
+        res.si <- summary(si)
+        # create output
+        r <- data.frame(enum.id=as.numeric(names(res.si$clus.avg.widths)), si=res.si$clus.avg.widths) %>% 
+          left_join(distinct(select(df, !!sym(col_admin), !!sym(col_enum), enum.id)), by="enum.id") %>% 
+          left_join(group_by(df, enum.id) %>% summarise(num.surveys=n(), .groups="drop_last"), by="enum.id") %>% 
+          select(!!sym(col_admin), !!sym(col_enum), num.surveys, si) %>% 
+          arrange(-si)
+        return(r)
+      }
+    })
+  do.call(rbind, res)
 }
