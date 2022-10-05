3b61ab7415d1ec66cddc201a9c456b8ba2b90a79
Anthony Twesigye <twesigye10@gmail.com>
2022-10-05 19:37:01 +0300

    testing_data

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -44,6 +44,30 @@ df_sample_data <- sf::st_read("inputs/livelihoods_settlement_host_samples.gpkg",
 
 logic_output <- list()
 
+
+# data not meeting minimum requirements -----------------------------------
+
+# testing_data
+df_testing_data <- df_tool_data %>% 
+  filter(i.check.start_date < as_date("2022-10-06") | str_detect(string = hh_id, pattern = fixed('test', ignore_case = TRUE))) %>% 
+  mutate(i.check.type = "remove_survey",
+         i.check.name = "",
+         i.check.current_value = "",
+         i.check.value = "",
+         i.check.issue_id = "logic_c_testing_data",
+         i.check.issue = "testing_data",
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "1",
+         i.check.adjust_log = "",
+         i.check.so_sm_choices = "") %>% 
+  dplyr::select(starts_with("i.check.")) %>% 
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+
+add_checks_data_to_list(input_list_name = "logic_output", input_df_name = "df_testing_data")
+
 # time checks -------------------------------------------------------------
 
 # Time interval for the survey
