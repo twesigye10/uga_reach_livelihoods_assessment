8aafde6305ce93fca43c80c77ef592827d0bb6e9
Anthony Twesigye <twesigye10@gmail.com>
2022-11-12 18:31:17 +0300

    sorcing functions and column selection for loops

--- R/Cleaning_implimentor.R
+++ R/Cleaning_implimentor.R
@@ -1,17 +1,15 @@
 
 # Applying the cleaning log to clean the data
-
 library(tidyverse)
 library(lubridate)
 library(glue)
-library(cleaninginspectoR)
-library(kobold)
+
+source("R/support_functions.R")
 
 # Read data and checking log
 
 df_cleaning_log <- read_csv("inputs/combined_checks_livelihood.csv", col_types = cols(sheet = "c", index = "i")) %>% 
   filter(!adjust_log %in% c("delete_log")) %>%
-  mutate(relevant = NA) %>% 
   mutate(adjust_log = ifelse(is.na(adjust_log), "apply_suggested_change", adjust_log),
          value = ifelse(is.na(value) & str_detect(string = issue_id, pattern = "logic_c_"), "blank", value),
          value = ifelse(type %in% c("remove_survey"), "blank", value),
@@ -27,16 +25,16 @@ data_path <- "inputs/livelihoods_assessment_data.xlsx"
 # main data
 cols_to_escape <- c("index", "start", "end", "today", "starttime",	"endtime", "_submission__submission_time")
 
-data_nms <- names(readxl::read_excel(path = data_path, n_max = 500))
+data_nms <- names(readxl::read_excel(path = data_path, n_max = 2000))
 c_types <- ifelse(str_detect(string = data_nms, pattern = "_other$"), "text", "guess")
 
 df_raw_data <- readxl::read_excel(path = data_path, col_types = c_types) %>% 
   filter(as_date(as_datetime(start)) > as_date("2022-08-10")) %>%
   mutate(across(.cols = -c(contains(cols_to_escape)), 
                 .fns = ~ifelse(str_detect(string = ., 
-                                          pattern = fixed(pattern = "N/A", ignore_case = TRUE)), "NA", .))) %>% 
-  mutate(across(.cols = -c(contains(cols_to_escape), matches("_age$|^age_|uuid")), 
-                .fns = ~ifelse(str_detect(string = ., pattern = "^[9]{2,9}$"), "NA", .)))
+                                          pattern = fixed(pattern = "N/A", ignore_case = TRUE)), "NA", .))) #%>% 
+  # mutate(across(.cols = -c(contains(cols_to_escape), matches("_age$|^age_|uuid")), 
+  #               .fns = ~ifelse(str_detect(string = ., pattern = "^[9]{2,9}$"), "NA", .)))
 
 # loops
 hh_roster <- readxl::read_excel(path = data_path, sheet = "hh_roster")
@@ -56,13 +54,12 @@ df_choices <- readxl::read_excel("inputs/livelihoods_assessment_tool.xlsx", shee
 
 
 # Find all new choices to add to sheet ------------------------------------
+df_cleaning_log_main <- df_cleaning_log %>% 
+  filter(is.na(sheet))
 
 df_grouped_choices<- df_choices %>% 
   group_by(list_name) %>% 
   summarise(choice_options = paste(name, collapse = " : "))
-
-df_cleaning_log_main <- df_cleaning_log %>% 
-  filter(is.na(sheet))
 
 # get new name and ad_option pairs to add to the choices sheet
 new_vars <- df_cleaning_log_main %>% 
@@ -76,7 +73,7 @@ new_vars <- df_cleaning_log_main %>%
   select(name, choice) %>%
   distinct() %>% # to make sure there are no duplicates
   arrange(name)
-
+ 
 # create kobold object ----------------------------------------------------
 
 kbo <- kobold::kobold(survey = df_survey, 
@@ -157,19 +154,23 @@ for (cur_sm_col in sm_colnames) {
   df_handle_sm_data <- df_updated_data
 }
 
-df_final_cleaned_data <- df_handle_sm_data
-
-
+df_cleaned_data <- df_handle_sm_data
 
 
 # clean repeats -----------------------------------------------------------
+other_repeat_col <- c("start", "end", "today", "consent_one", "consent_two", "hoh_equivalent", "respondent_gender", 
+                      "respondent_age", "respondent_education", "gender_hoh", "age_hoh", 
+                      "education_hoh", "location", "location_type", "status_intro", "ctry_origin_not_uganda", 
+                      "ctry_origin_not_uganda_other", "date_arrival", "hh_living_status", "town_hh_living_in", 
+                      "settlement_name", "status")
 df_cleaning_log_roster <- df_cleaning_log %>% 
   filter(!is.na(sheet), uuid %in% df_raw_data_hh_roster$`_uuid`, name %in% colnames(df_raw_data_hh_roster))
 
 df_cleaned_data_hh_roster <- implement_cleaning_support(input_df_raw_data = df_raw_data_hh_roster,
                                                       input_df_survey = df_survey,
                                                       input_df_choices = df_choices,
-                                                      input_df_cleaning_log = df_cleaning_log_roster)
+                                                      input_df_cleaning_log = df_cleaning_log_roster) %>% 
+  select(other_repeat_col, any_of(colnames(hh_roster)), `_index` = index, `_submission__uuid` = uuid)
 
 df_cleaning_log_school <- df_cleaning_log %>% 
   filter(!is.na(sheet), uuid %in% df_raw_data_hh_repeat_school_enrollment$`_uuid`, name %in% colnames(df_raw_data_hh_repeat_school_enrollment))
@@ -177,12 +178,13 @@ df_cleaning_log_school <- df_cleaning_log %>%
 df_clean_data_hh_repeat_school_enrollment <- implement_cleaning_support(input_df_raw_data = df_raw_data_hh_repeat_school_enrollment,
                                                                         input_df_survey = df_survey,
                                                                         input_df_choices = df_choices,
-                                                                        input_df_cleaning_log = df_cleaning_log_school)
+                                                                        input_df_cleaning_log = df_cleaning_log_school) %>% 
+  select(other_repeat_col, any_of(colnames(hh_repeat_school_enrollment)), `_index` = index, `_submission__uuid` = uuid)
 
 
 # write final modified data -----------------------------------------------
 
-list_of_clean_datasets <- list("UGA2205_HH Livelihood" = df_final_cleaned_data,
+list_of_clean_datasets <- list("UGA2205_HH Livelihood" = df_cleaned_data,
                                "hh_roster" = df_cleaned_data_hh_roster,
                                "hh_repeat_school_enrollment" = df_clean_data_hh_repeat_school_enrollment
 )
